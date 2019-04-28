044f813a1aeda17da0389968476038cc13593cd3
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-04-27 21:23:16 -0700

    Update News.php

--- SP19/application/controllers/News.php
+++ SP19/application/controllers/News.php
@@ -3,19 +3,21 @@
 
 class News extends CI_Controller {
 
-    public function __construct() {
+    public function __construct() 
+    {
         parent::__construct();
         $this->load->model('news_model');
         $this->load->helper('url_helper');
     }
 
-    public function index() {
+    public function index() 
+    {
         $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
 
-        $this->load->view('templates/header', $data);
+        //$this->load->view('templates/header', $data);
         $this->load->view('news/index', $data);
-        $this->load->view('templates/footer', $data);
+        //$this->load->view('templates/footer', $data);
     }
 
     public function view($slug = NULL) {
