256bc4f8afd4319595303ebf4970c9f5394c6886
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-04-30 12:03:54 -0700

    Update custom_config.php

--- SP19/application/config/custom_config.php
+++ SP19/application/config/custom_config.php
@@ -1,9 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 //custom_config.php - a place to save global settings
-
-$config['style'] = 'amelia.css';
+$config['style'] = 'spacelab.css';
 $config['banner'] = 'Default Banner';
 $config['title'] = 'Default Title';
-$config['copyright'] = 'Default Copyright';
-$config['masthead'] = 'Default MastHead';
+$config['copyright'] = 'Odis Mills Entertainment';
+$config['masthead'] = 'SSOM';
 $config['theme'] = 'themes/bootswatch/';
+$config['nav1'] = array(
+    'news' => 'News',
+    'news/create' => 'Create News'
+);
