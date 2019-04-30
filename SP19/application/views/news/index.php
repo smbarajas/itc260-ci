be7c68b7ae56cbce3498dda4c733874412c5edec
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-04-30 12:20:58 -0700

    Update index.php

--- SP19/application/views/news/index.php
+++ SP19/application/views/news/index.php
@@ -2,9 +2,8 @@
 // application/views/news/index.php
 
 $this->load->view($this->config->item('theme') . 'header');
-
-
 ?>
+
 <h2><?php echo $title; ?></h2>
 
 <?php foreach ($news as $news_item): ?>
