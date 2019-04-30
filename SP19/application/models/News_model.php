1b3c52ddfb773bdcc257ff1d96ec14b795c55c6f
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-04-30 12:17:17 -0700

    Update News_model.php

--- SP19/application/models/News_model.php
+++ SP19/application/models/News_model.php
@@ -1,11 +1,11 @@
 <?php
 // application/models/News_model.php
 class News_model extends CI_Model {
-
+        
         public function __construct() {
                 $this->load->database();
         }
-
+        
         public function get_news($slug = FALSE) {
                 if ($slug === FALSE) {
                         $query = $this->db->get('SP19_news');
@@ -14,18 +14,22 @@ class News_model extends CI_Model {
                 $query = $this->db->get_where('SP19_news', array('slug' => $slug));
                 return $query->row_array();
         }
-
+        
         public function set_news() {
                 $this->load->helper('url');
-
+                
                 $slug = url_title($this->input->post('title'), 'dash', TRUE);
-
+                
                 $data = array(
                         'title' => $this->input->post('title'),
                         'slug' => $slug,
                         'text' => $this->input->post('text')
                 );
-
-                return $this->db->insert('SP19_news', $data);
+                // return $this->db->insert('SP19_news', $data);
+                if($this->db->insert('SP19_news', $data)) { // return slug
+                        return $slug;
+                } else { // return false
+                        return false;
+                }
         }
 }
