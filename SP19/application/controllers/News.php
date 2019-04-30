8f9eabcde6d52fa1c740de8e11d4973fb8316df9
smbarajas <32750643+smbarajas@users.noreply.github.com>
2019-04-30 12:06:32 -0700

    Update News.php

--- SP19/application/controllers/News.php
+++ SP19/application/controllers/News.php
@@ -1,58 +1,57 @@
 <?php
 // application/controllers/News.php
-
 class News extends CI_Controller {
-
-    public function __construct() 
-    {
+    public function __construct() {
         parent::__construct();
         $this->load->model('news_model');
         $this->load->helper('url_helper');
+        // Bad News Dudes
+        $this->config->set_item('banner', 'Bad News Dudes');
     }
-
-    public function index() 
-    {
+    public function index() {
+        // Replace default title with 'this->' title
+        $this->config->set_item('title', 'Bad News Dudes');
+        // Nav Links
+        $nav1 = $this->config->item('nav1');
+        // var_dump($nav1);
+        // die;
         $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
-
-        //$this->load->view('templates/header', $data);
         $this->load->view('news/index', $data);
-        //$this->load->view('templates/footer', $data);
     }
-
     public function view($slug = NULL) {
+        // slug without dashes
+        $dashless_slug = str_replace("-", " ", $slug);
+        // uppercase slug words
+        $dashless_slug = ucwords($dashless_slug);
+        // Use dashless slug for title
+        $this->config->set_item('title', 'Newsflash! - ' . $dashless_slug);
         $data['news_item'] = $this->news_model->get_news($slug);
-
         if (empty($data['news_item'])) {
             show_404();
         }
-
         $data['title'] = $data['news_item']['title'];
-
-        $this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
-        $this->load->view('templates/footer', $data);
     }
-
     public function create() {
         $this->load->helper('form');
         $this->load->library('form_validation');
-
         $data['title'] = 'Create a news item';
-
         $this->form_validation->set_rules('title', 'Title', 'required');
         $this->form_validation->set_rules('text', 'Text', 'required');
-
         if ($this->form_validation->run() === FALSE) {
-            $this->load->view('templates/header', $data);
             $this->load->view('news/create', $data);
-            $this->load->view('templates/footer', $data);
-
         } else {
-            $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
-            $this->load->view('news/success', $data);
-            $this->load->view('templates/footer', $data);
+            // $this->news_model->set_news();
+            // $this->load->view('news/success', $data);
+            $slug = $this->news_model->set_news();
+            if($slug!==false) { // slug sent
+                feedback('Data entered successfully', 'info');
+                redirect('news/view/' . $slug);
+            } else { // error
+                feedback('Data NOT entered!', 'error');
+                redirect('news/create');
+            }
         }
     }
 }
